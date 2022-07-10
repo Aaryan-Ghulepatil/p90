19d613597f876a069ab186c8181fa60560ce8b0a
Aaryan-Ghulepatil <95683837+Aaryan-Ghulepatil@users.noreply.github.com>
2022-07-10 15:02:34 +0530

    Add files via upload

---
+++ quiz_game_page.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+
+
+function send(){
+    number1=document.getElementById("number1").value;
+    number2=document.getElementById("number2").value;
+    actualanswer=parseInt(number1)*parseInt(number2);
+}
